86d765d3297bcf3b0c4ce8aa7476f49830d64c4f
SaifBhuiya <bhuiyasaif@gmail.com>
2024-10-14 08:07:10 +0600

    path

--- buetcsefest2024/Scripts/Player.gd
+++ buetcsefest2024/Scripts/Player.gd
@@ -1,63 +1,48 @@
 extends Camera3D
+var speed = 0
 
-@export var mouse_sensitivity = 0.05
-@export var walk_speed = 5.0
-@export var run_speed = 8.0
-@export var jump_force = 5.0
-@export var gravity = -9.8
 
-var velocity = Vector3.ZERO
-var speed = walk_speed
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_parent().get_node("Path").get_child(0).get_node("PathFollower").get_child(0).remote_path = self.get_path()
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
-		rotation.x = clamp(rotation.x - deg_to_rad(event.relative.y * mouse_sensitivity), 
-						   deg_to_rad(-89), 
-						   deg_to_rad(89))
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:			
+		
 	
-	if event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-func _physics_process(delta):
-	# Add gravity
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if (Input.is_action_just_pressed("move_forward")):
+		speed = 1
+		self.fov += 2
+	if (Input.is_action_just_pressed("move_back")):
+		speed = 0
+		self.fov -= 2
+		
+			
+		
+				
+	if (Input.is_action_pressed("Right")):
+		if(get_parent().get_node("Path").get_child(0).get_node("PathFollower").h_offset <= max):
+			get_parent().get_node("Path").get_child(0).get_node("PathFollower").h_offset += 1
+		
+	if (Input.is_action_pressed("Left")):
+		if( get_parent().get_node("Path").get_child(0).get_node("PathFollower").h_offset >= min):
+			get_parent().get_node("Path").get_child(0).get_node("PathFollower").h_offset -= 1
 	
-	# Handle jumping
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_force
+	get_parent().get_node("Path").get_child(0).get_node("PathFollower").progress += speed
+	print(speed)
 	
-	# Handle running
-	if Input.is_action_pressed("run"):
-		speed = run_speed
-	else:
-		speed = walk_speed
+#
+#func _input(event):	
+	#
+	#if(event is InputEventMouseMotion):
+		#
+		#rotate_y(deg_to_rad(-event.relative.x * .1))
+		#rotation.y = clamp (rotation.y,deg_to_rad(-15),deg_to_rad(15))
 	
-	# Get input direction
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	
-	# Calculate movement direction
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	# Apply movement
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-	
-	# Move the camera
-	global_translate(velocity * delta)
-	
-	# Simple floor check (you might want to use raycasts for more precise detection)
-	if global_position.y < 0:
-		global_position.y = 0
-		velocity.y = 0
-
-func is_on_floor():
-	return global_position.y <= 0
+		
+		
+#func _on_whiplash_timeout() -> void:
+	#self.fov = 75
+	##speed = 0
+	#pass # Replace with function body.
